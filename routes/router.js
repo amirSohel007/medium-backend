d3aac0f187182a31aeebf5bd854d95bde6039165
amirSohel007 <amirsohel27@gmail.com>
2020-05-29 08:39:47 +0530

    remove test route

--- routes/router.js
+++ routes/router.js
@@ -24,8 +24,4 @@ routes.get('/users', getUsers)
 //single user
 routes.get('/user/:id', singleUser)
 
-routes.get('/testing', (req, res) => {
-    res.json({status:'application working !'})
-})
-
 module.exports = routes;
